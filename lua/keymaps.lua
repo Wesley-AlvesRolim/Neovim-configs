3ad286974a8675a97b5fb31631843c2914d6a114
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-10-25 19:11:04 -0300

    feat: select the option by coc intellisense using TAB

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,3 +15,12 @@ vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
 
+-- Coc
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#confirm() : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+
