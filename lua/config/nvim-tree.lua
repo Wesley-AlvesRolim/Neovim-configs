5e038aabdba95e0fca0b7614a260308b52948f54
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-02-04 16:43:17 -0300

    fix: force nvim-tree open on VimEnter

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -68,7 +68,8 @@ require("nvim-tree").setup({
         symlink_destination = true
     },
     filters = {
-        dotfiles = false
+        dotfiles = false,
+				custom = {".git", "node_modules"}
     }
 })
 
@@ -89,5 +90,26 @@ require('nvim-web-devicons').setup {
     default = true
 }
 
-vim.api.nvim_exec([[ NvimTreeFocus ]], false)
+local function open_nvim_tree(data)
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
