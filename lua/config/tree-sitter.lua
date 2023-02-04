fa8e71c929d42a842a4289653b5e2234830e92bb
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-02-04 16:23:45 -0300

    feat: add incremental_selection  to nvim-treesitter

--- lua/config/tree-sitter.lua
+++ lua/config/tree-sitter.lua
@@ -1,16 +1,27 @@
 require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "java", "typescript", "yaml", "json", "python", "regex", "rust", "tsx" },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = { "javascript", "scss" },
+    -- A list of parser names, or "all" (the four listed parsers should always be installed)
+    ensure_installed = {"c", "lua", "vim", "help", "java", "typescript", "yaml", "json", "python", "regex", "rust",
+                        "tsx"},
+    sync_install = false,
+    auto_install = true,
+    ignore_install = {"javascript", "scss"},
 
-	indent = {
-    enable = true
-  },
+    indent = {
+        enable = true
+    },
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<c-backspace>'
+        }
+    },
+
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    }
 })
