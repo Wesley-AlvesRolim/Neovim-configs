3441580cadea698041f6cc7d1355c391031edd12
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-06-05 14:13:14 -0300

    feat: add keymap to Comment

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,31 +1,30 @@
+local keymap = vim.keymap.set
+
 -- paste "'"
-vim.api.nvim_set_keymap("i", "<C-v>", "'", { silent = true })
+keymap("i", "<C-v>", "'", { silent = true })
 
 -- Nvim.Tree
-vim.api.nvim_set_keymap("n", "<C-B>", ":NvimTreeToggle<CR>", { silent = true })
-vim.api.nvim_set_keymap("i", "<C-B>", "<C-c>:NvimTreeToggle<CR>", { silent = true })
+keymap("n", "<C-B>", ":NvimTreeToggle<CR>", { silent = true })
+keymap("i", "<C-B>", "<C-c>:NvimTreeToggle<CR>", { silent = true })
 
--- save CTRL + s
-vim.api.nvim_set_keymap("i", "<C-s>", "<C-c>:w<CR>", { silent = true })
+-- save
+keymap("n", "<leader>w", ":w<CR>", { silent = true })
+keymap("i", "<C-s>", "<C-c>:w<CR>", { silent = true })
 
 -- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer]' })
+local builtin = require("telescope.builtin")
+keymap("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+keymap("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+keymap("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer]" })
 
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+keymap("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+keymap("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+keymap("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+keymap("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+keymap("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 
--- Coc
-function _G.check_back_space()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#confirm() : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-
+-- Comment
+keymap("v", "<leader>/",
+	"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+	{ desc = "Toggle comment for selection" }
+)
