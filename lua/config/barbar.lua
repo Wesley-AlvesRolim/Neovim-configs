73ff780b98868fd3fccc451630d53aa784f40426
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-06-06 13:27:37 -0300

    refactor: better setup to bufferline

--- lua/config/barbar.lua
+++ lua/config/barbar.lua
@@ -1,3 +1,4 @@
+local icons = require("config.icons")
 local nvim_tree_events = require("nvim-tree.events")
 local bufferline_api = require("bufferline.api")
 
@@ -18,45 +19,28 @@ nvim_tree_events.subscribe("TreeClose", function()
 end)
 
 require("bufferline").setup({
+
 	-- Enable/disable animations
 	animation = true,
 
 	-- Enable/disable auto-hiding the tab bar when there is a single buffer
 	auto_hide = true,
 
-	-- Enable/disable current/total tabpages indicator (top right corner)
-	tabpages = true,
-
 	-- Enable/disable close button
 	closable = true,
 
-	-- Enables/disable clickable tabs
-	--  - left-click: go to buffer
-	--  - middle-click: delete buffer
-	clickable = true,
-
-	-- Disable highlighting alternate buffers
-	highlight_alternate = false,
-
-	-- Enable highlighting visible buffers
-	highlight_visible = true,
-
 	icons = {
-		-- Configure the base icons on the bufferline.
-		-- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
-		buffer_index = false,
-		buffer_number = false,
 		-- Enables / disables diagnostic symbols
 		diagnostics = {
-			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
+			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = icons.diagnostics.Error },
 			[vim.diagnostic.severity.WARN] = { enabled = false },
 			[vim.diagnostic.severity.INFO] = { enabled = false },
-			[vim.diagnostic.severity.HINT] = { enabled = true },
+			[vim.diagnostic.severity.HINT] = { enabled = true, icon = icons.diagnostics.Hint },
 		},
 		gitsigns = {
-			added = { enabled = true, icon = "+" },
-			changed = { enabled = true, icon = "~" },
-			deleted = { enabled = true, icon = "-" },
+			added = { enabled = true, icon = icons.git.add.text },
+			changed = { enabled = true, icon = icons.git.change.text },
+			deleted = { enabled = true, icon = icons.git.delete.text },
 		},
 		filetype = {
 			-- Sets the icon's highlight group.
@@ -66,28 +50,14 @@ require("bufferline").setup({
 			-- Requires `nvim-web-devicons` if `true`
 			enabled = true,
 		},
+
 		separator = { left = "▎", right = "" },
 
 		-- Configure the icons on the bufferline when modified or pinned.
 		-- Supports all the base icon options.
 		modified = { button = "●" },
-		pinned = { button = "", filename = true },
-
-		-- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
-		preset = "default",
-
-		-- Configure the icons on the bufferline based on the visibility of a buffer.
-		-- Supports all the base icon options, plus `modified` and `pinned`.
-		alternate = { filetype = { enabled = false } },
-		current = { buffer_index = true },
-		inactive = { button = "×" },
-		visible = { modified = { buffer_number = false } },
+		pinned = { button = "", filename = true },
 	},
-
-	-- If true, new buffers will be inserted at the start/end of the list.
-	-- Default is to insert after current buffer.
-	insert_at_end = false,
-	insert_at_start = false,
 
 	-- Sets the maximum padding width with which to surround each tab
 	maximum_padding = 1,
@@ -98,20 +68,14 @@ require("bufferline").setup({
 	-- Sets the maximum buffer name length.
 	maximum_length = 30,
 
-	-- If set, the letters for each buffer in buffer-pick mode will be
-	-- assigned based on their name. Otherwise or in case all letters are
-	-- already assigned, the behavior is to assign letters in order of
-	-- usability (see order below)
-	semantic_letters = true,
+	-- Sets the minimum buffer name length.
+	minimum_length = 10,
 
-	-- New buffer letters are assigned in this order. This order is
-	-- optimal for the qwerty keyboard layout but might need adjustement
-	-- for other layouts.
-	letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-
-	-- Sets the name of unnamed buffers. By default format is "[Buffer X]"
-	-- where X is the buffer number. But only a static string is accepted here.
-	no_name_title = nil,
+	-- Set the filetypes which barbar will offset itself for
+	sidebar_filetypes = {
+		-- Use the default values: {event = 'BufWinLeave', text = nil}
+		NvimTree = true,
+	},
 })
 
 local map = vim.api.nvim_set_keymap
@@ -123,40 +87,9 @@ local opts = {
 -- Move to previous/next
 map("n", "<S-H>", "<Cmd>BufferPrevious<CR>", opts)
 map("n", "<S-L>", "<Cmd>BufferNext<CR>", opts)
--- Re-order to previous/next
--- map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
--- map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
--- map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
--- map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
--- map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
--- map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
--- map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
--- map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
--- map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
--- map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
--- map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
--- map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
--- map('n', '<C-p>', '<Cmd>BufferPin<CR>', opts)
--- Close buffer
 map("n", "<C-C>", "<Cmd>BufferClose<CR>", opts)
--- Wipeout buffer
---                 :BufferWipeout
--- Close commands
---                 :BufferCloseAllButCurrent
---                 :BufferCloseAllButPinned
---                 :BufferCloseAllButCurrentOrPinned
---                 :BufferCloseBuffersLeft
---                 :BufferCloseBuffersRight
--- Magic buffer-picking mode
--- map('n', '<A-p>', '<Cmd>BufferPick<CR>', opts)
 -- Sort automatically by...
 map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
 map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
 map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
 map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
-
--- Other:
--- :BarbarEnable - enables barbar (enabled by default)
--- :BarbarDisable - very bad command, should never be used
