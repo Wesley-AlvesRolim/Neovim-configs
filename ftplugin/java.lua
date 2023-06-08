9715f615fd788bbbdf193c427f454eb4437bdd19
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-06-08 13:33:07 -0300

    style: format

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,6 @@
 local config = {
-    cmd = {vim.fs.normalize('~/.local/share/nvim/jdt-language-server/bin/jdtls')},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1])
+	cmd = { vim.fs.normalize("~/.local/share/nvim/jdt-language-server/bin/jdtls") },
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
 
-require('jdtls').start_or_attach(config)
-
+require("jdtls").start_or_attach(config)
