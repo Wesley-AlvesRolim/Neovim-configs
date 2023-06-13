5ba950f47d37a34d418014b3c961b070c08ab806
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-06-13 00:15:24 -0300

    fix: did not start the lsp servers

--- lua/lsp/configs.lua
+++ lua/lsp/configs.lua
@@ -1,21 +1,25 @@
 local configs = require("lsp.handlers")
 local M = {}
 
+local function is_configured_servers(server_to_compare, configured_servers)
+	for _, configured_server in ipairs(configured_servers) do
+		return configured_server == server_to_compare
+	end
+end
+
 M.setup = function(servers, configured_servers)
-	for _, server in pairs(servers) do
-		for _, configured_server in pairs(configured_servers) do
-			if server == configured_server then
-				return
-			end
+	for _, server in ipairs(servers) do
+		if not is_configured_servers(server, configured_servers) then
+			local opts = {
+				on_attach = configs.on_attach,
+				capabilities = require("cmp_nvim_lsp").default_capabilities(
+					vim.lsp.protocol.make_client_capabilities()
+				),
+			}
+
+			require("lspconfig")[server].setup(opts)
+			configs.setup()
 		end
-
-		local opts = {
-			on_attach = configs.on_attach,
-			capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-		}
-
-		require("lspconfig")[server].setup(opts)
-		configs.setup()
 	end
 end
 
