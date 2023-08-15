15d06ab8ed1ff5049c69262345e855057db5cd62
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-15 17:59:40 -0300

    feat: add fzf native to telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,7 +36,17 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.1",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build =
+				"cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				config = function()
+					require("telescope").load_extension("fzf")
+				end,
+			},
+		},
 	},
 
 	-- Git
