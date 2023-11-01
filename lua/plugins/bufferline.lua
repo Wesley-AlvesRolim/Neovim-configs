f15b13a4e18bedce2dcb677ff7518007e3d42977
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-11-01 12:50:24 -0300

    chore: fixing the commit of nvim-cokeline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 return {
 	"willothy/nvim-cokeline",
 	event = "VeryLazy",
+	commit = "48636776059b5fb6de4b91a21655b06d16150ce6",
 	dependencies = {
 		"nvim-lua/plenary.nvim",     -- Required for v0.4.0+
 		"nvim-tree/nvim-web-devicons", -- If you want devicons
