e51b0f76fcd045df22334a5966a8849596c383bc
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-12-21 13:47:48 -0300

    refactor: move telescope to it own file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,22 +27,6 @@ return {
     end,
   },
 
-  -- Files finder
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-        config = function()
-          require("telescope").load_extension("fzf")
-        end,
-      },
-    },
-  },
-
   -- Code Copilot
   {
     "Exafunction/codeium.nvim",
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
