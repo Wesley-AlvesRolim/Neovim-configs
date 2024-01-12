f267c17f601446b02a8679c0a8a8f99b1f13b124
Wesley Alves Rolim <walvesrolim@gmail.com>
2024-01-12 17:37:18 -0300

    feat: add dap config to cs

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -1,12 +1,36 @@
 local opts = require("plugins.lsp.lspconfig.handlers").opts
+local pid = vim.fn.getpid()
 
-local omnisharp_bin = vim.fn.stdpath("data") .. "/mason/bin/omnisharp-mono"
+local mason_bin_dir = vim.fn.stdpath("data") .. "/mason/bin/"
+local omnisharp_bin = mason_bin_dir .. "omnisharp"
 
-require("lspconfig")["omnisharp_mono"].setup({
-  cmd = { omnisharp_bin },
+require("lspconfig")["omnisharp"].setup({
+  cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
   capabilities = opts.capabilities,
   on_attach = function(client, bufnr)
     opts.on_attach(nil, bufnr)
     client.server_capabilities.semanticTokensProvider = nil
+
+    local dap = require("dap")
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = mason_bin_dir .. "netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+          local ddl = vim.fn.getcwd() .. "/bin/Debug/"
+          vim.ui.input({ prompt = "Path to dll: " .. ddl }, function(input)
+            ddl = ddl .. input
+          end)
+          return ddl
+        end,
+      },
+    }
   end,
 })
