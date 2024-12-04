fc9dfc41f4da85d789d330282daf6151477b3f7d
Wesley Alves Rolim <walvesrolim@gmail.com>
2024-12-04 12:06:36 -0300

    feat: add biome

--- lua/plugins/toolkit/source.lua
+++ lua/plugins/toolkit/source.lua
@@ -1,83 +1,104 @@
 local M = {}
 
 M.formatters = {
-  markdownlint = {
-    filetype = {
-      "markdown",
-    },
-  },
-  stylua = {
-    filetype = {
-      "lua",
-    },
-  },
-  -- prettierd = {
-  --   filetype = {
-  --     "javascript",
-  --     "javascriptreact",
-  --     "typescript",
-  --     "typescriptreact",
-  --     "vue",
-  --     "html",
-  --     "css",
-  --     "json",
-  --     "jsonc",
-  --     -- "yaml",
-  --   },
-  -- },
-  gdformat = {
-    command = vim.fn.stdpath("data") .. "/mason/bin/gdformat",
-    filetype = {
-      "gd",
-      "gdscript",
-      "gdscript3",
-    },
-  },
-  csharpier = {
-    filetype = {
-      "cs",
-    },
-  },
-  ["pretty-php"] = {
-    filetype = {
-      "php",
-    },
-  },
-  blue = {
-    filetype = {
-      "python",
-    },
-  },
+	markdownlint = {
+		filetype = {
+			"markdown",
+		},
+	},
+	stylua = {
+		filetype = {
+			"lua",
+		},
+	},
+	biome = {
+		filetype = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"html",
+			"css",
+			"json",
+			"jsonc",
+			"yaml",
+		},
+	},
+	-- prettierd = {
+	--   filetype = {
+	--     "javascript",
+	--     "javascriptreact",
+	--     "typescript",
+	--     "typescriptreact",
+	--     "vue",
+	--     "html",
+	--     "css",
+	--     "json",
+	--     "jsonc",
+	--     -- "yaml",
+	--   },
+	-- },
+	gdformat = {
+		command = vim.fn.stdpath("data") .. "/mason/bin/gdformat",
+		filetype = {
+			"gd",
+			"gdscript",
+			"gdscript3",
+		},
+	},
+	csharpier = {
+		filetype = {
+			"cs",
+		},
+	},
+	["pretty-php"] = {
+		filetype = {
+			"php",
+		},
+	},
+	blue = {
+		filetype = {
+			"python",
+		},
+	},
 }
 
 M.linters = {
-  markdownlint = {
-    filetype = {
-      "markdown",
-    },
-  },
-  -- eslint_d = {
-  --   filetype = {
-  --     "javascript",
-  --     "javascriptreact",
-  --     "typescript",
-  --     "typescriptreact",
-  --     "vue",
-  --   },
-  -- },
-  gdlint = {
-    command = vim.fn.stdpath("data") .. "/mason/bin/gdlint",
-    filetype = {
-      "gd",
-      "gdscript",
-      "gdscript3",
-    },
-  },
+	markdownlint = {
+		filetype = {
+			"markdown",
+		},
+	},
+	biomejs = {
+		filetype = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+		},
+	},
+	-- eslint_d = {
+	--   filetype = {
+	--     "javascript",
+	--     "javascriptreact",
+	--     "typescript",
+	--     "typescriptreact",
+	--     "vue",
+	--   },
+	-- },
+	gdlint = {
+		command = vim.fn.stdpath("data") .. "/mason/bin/gdlint",
+		filetype = {
+			"gd",
+			"gdscript",
+			"gdscript3",
+		},
+	},
 }
 
 M.alternative_package = {
-  gdformat = "gdtoolkit",
-  gdlint = "gdtoolkit",
+	gdformat = "gdtoolkit",
+	gdlint = "gdtoolkit",
 }
 
 return M
