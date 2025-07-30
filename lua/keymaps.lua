c4676bbd446b25155d0a0d1914a0668b2e260d0b
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-07-30 08:51:23 -0300

    feat: add new keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,16 +1,16 @@
 local merge = require("utils").merge
 
 local function keymap(mode, keys, callback, opts)
-  local default_opts = {
-    silent = true,
-    noremap = true,
-  }
-  if type(opts) == "table" then
-    opts = merge(default_opts, opts)
-  else
-    opts = default_opts
-  end
-  vim.keymap.set(mode, keys, callback, opts)
+	local default_opts = {
+		silent = true,
+		noremap = true,
+	}
+	if type(opts) == "table" then
+		opts = merge(default_opts, opts)
+	else
+		opts = default_opts
+	end
+	vim.keymap.set(mode, keys, callback, opts)
 end
 
 -- quit
@@ -37,15 +37,21 @@ keymap("x", "<A-k>", ":m '<-2<CR>gv-gv")
 keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
 keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 
+-- Center screen when jumping
+keymap("n", "n", "nzz", { desc = "Next search result (centered)" })
+keymap("n", "N", "Nzz", { desc = "Previous search result (centered)" })
+keymap("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+keymap("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
 -- Telescope
 local builtin = require("telescope.builtin")
 keymap("n", "<leader>so", builtin.oldfiles, { desc = "[S]earch [O]ld files: Find recently opened files" })
 keymap("n", "<leader>sb", builtin.buffers, { desc = "[S]earch Buffers: Find existing buffers" })
 keymap(
-  "n",
-  "<leader>sz",
-  builtin.current_buffer_fuzzy_find,
-  { desc = "[S]earch Fu[z]zilly: Fuzzily search in current buffer]" }
+	"n",
+	"<leader>sz",
+	builtin.current_buffer_fuzzy_find,
+	{ desc = "[S]earch Fu[z]zilly: Fuzzily search in current buffer]" }
 )
 
 keymap("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
@@ -54,10 +60,10 @@ keymap("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord
 keymap("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 keymap("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 keymap(
-  "n",
-  "<leader>sa",
-  "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
-  { desc = "[S]earch [A]ll" }
+	"n",
+	"<leader>sa",
+	"<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+	{ desc = "[S]earch [A]ll" }
 )
 keymap("n", "<leader>sh", builtin.help_tags, { desc = "Help page" })
 keymap("n", "<leader>cm", builtin.git_commits, { desc = "Git [c]o[m]mits" })
