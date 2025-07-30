88cc16447d172758becdcd026e6a3e4e6f50651a
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-07-30 08:51:16 -0300

    refactor: update `telescope` and setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,16 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.5",
+  tag = "0.1.8",
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     {
